e20992f904f6d70e148a39662900c9990003cee6
twolakes <brian@twolakessolutions.com>
2020-12-29 00:49:33 -0600

    added basic http functionality, tested sat w GET call

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ import requests
 import json
 import datetime
 
-from city_codes import get_city_code
+from helpers.weather_fs import get_weather
+from helpers.city_codes import get_city_code
 
 # load env variables
 from dotenv import load_dotenv
 load_dotenv()
 
 owm_key = os.getenv("OWM_KEY")
-# print(owm_key)        # <<<<<< REMOVE
+city_code = get_city_code('Cuenca-EQ')
+
+weather_data = get_weather(city_code, owm_key)
+print(weather_data)
 
 
 
